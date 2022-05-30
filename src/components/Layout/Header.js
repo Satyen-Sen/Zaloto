56106bc7fbd6b46e6368bfde546bd42b0b118232
Satyen-Sen <Satyendra.sant7@gmail.com>
2022-05-30 21:36:27 +0530

    updated header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>FoodieZone</h1>
+        <h1>Zoloto</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
